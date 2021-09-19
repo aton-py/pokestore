4f1f4465ea28027356021b0d0379c7cb4ed93a5f
Aton Bertini Dornfeld <68289323+aton-py@users.noreply.github.com>
2021-09-19 18:33:55 -0300

    🎮 Add Acess Allow to Api 🎮

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
   headers: {
-    "Access-Control-Allow-Origin": "",
+    "Access-Control-Allow-Origin": "https://pokestore-rho.vercel.app/",
   },
 });
 
